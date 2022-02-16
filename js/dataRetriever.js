bd818153dcf22c03b671aa0258536afa0c2a9bce
Yash <yashsood978@gmail.com>
2022-02-16 21:48:12 +0800

    setting up express.js environment

--- js/dataRetriever.js
+++ js/dataRetriever.js
@@ -1,3 +1,51 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Bitcoin is a cryptocurrency')
+})
+
+app.get('/coin/:coinID', (req, response) => {
+    getTickerIdFromMap(req.params["coinID"]).then(function (res) {
+        if (res != -1) {
+            //console.log(tickerToSearch + "'s CoinMarketCap ID is: " + res);
+            var tickerIdToSearch = res;
+            console.log("========================== DATA RECEIVED ==========================")
+            console.log(" ")
+            Promise.all([getCoinGeneralInfo(tickerIdToSearch), getCoinSecondaryInfo(tickerIdToSearch), getCoinLatestInfo(tickerIdToSearch)]).then((values) => {
+                //console.log(values[2]);
+                if(values[0] != -1 && values[1] != -1 && values[2] != -1){
+                    data_to_save["name"] = values[0]["name"];
+                    data_to_save["ticker"] = values[0]["symbol"];
+                    data_to_save["max_supply"] = values[0]["max_supply"]
+                    data_to_save["circulating_supply"] = values[0]["circulating_supply"]
+                    data_to_save["cmc_rank"] = values[0]["cmc_rank"];
+                    data_to_save["description"] = values[1]["description"]
+                    data_to_save["price"] = values[2]["quote"]["USD"]["price"]
+                    data_to_save["percent_change_1h"] = values[2]["quote"]["USD"]["percent_change_1h"]
+                    data_to_save["percent_change_24h"] = values[2]["quote"]["USD"]["percent_change_24h"]
+                    data_to_save["percent_change_7d"] = values[2]["quote"]["USD"]["percent_change_7d"]
+                    data_to_save["percent_change_30d"] = values[2]["quote"]["USD"]["percent_change_30d"]
+                    //console.log(data_to_save)
+                }
+                console.log("Data sent!")
+                response.send('Coin Price: ' + data_to_save["price"])
+            }).catch((reason) => {
+                console.log(reason);
+            });
+        } else {
+            console.log("Ticker does not exist");
+        }
+        
+    });
+
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 // Function to get CMC ID from ticker through APIs ==> https://pro-api.coinmarketcap.com/v1/cryptocurrency/map
 
 function getTickerIdFromMap(ticker) {
@@ -123,39 +171,100 @@ function getCoinSecondaryInfo(id) {
     });
 }
 
-// Function to get currencies historical data through APIs ==> GET /v1/cryptocurrency/category
+// Function to get currencies latest data through APIs ==> https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest
 
-function getCoinHistoricalInfo(id) {}
+function getCoinLatestInfo(id) {
+    const axios = require("axios");
 
-// Tester
-
-var tickerToSearch = "BTC";
-getTickerIdFromMap(tickerToSearch).then(function (res) {
-    if (res != -1) {
-        //console.log(tickerToSearch + "'s CoinMarketCap ID is: " + res);
-        var tickerIdToSearch = res;
-
-        getCoinGeneralInfo(tickerIdToSearch).then(function (res2) {
-            if (res2 != -1) {
-                console.log(
-                    res2["name"] + "'s General Data is: CMC ID: " + tickerIdToSearch + ", Ticker: " + res2["symbol"] + ", Max-Supply: " + 
-                        res2["max_supply"] + ", Rank: " + res2["cmc_rank"] //+ " and some random test: " + res2["last_updated"]
-                );
-            } else {
-                console.log("General info was not found :(");
+    let response = null;
+    let apiURL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+    return new Promise(async (resolve, reject) => {
+        try {
+            response = await axios.get(
+                apiURL,
+                {
+                    headers: {
+                        "X-CMC_PRO_API_KEY":
+                            "165f3a64-8fa3-425a-8717-933738c927af",
+                    },
+                }
+            );
+        } catch (ex) {
+            response = null;
+            // error
+            console.log("Error");
+            reject(ex);
+        }
+        if (response) {
+            // success
+            const json = response;
+            var returnedData = json;
+            var found = false;
+            for (var i = 0; i < returnedData.data.data.length; i++) {
+                if (id == returnedData.data.data[i]["id"]) {
+                    found = true;
+                    //console.log(output);
+                    //resolve(returnedData.data[i]["name"]);
+                    //console.log(returnedData.data.data[i])
+                    resolve(returnedData.data.data[i]);
+                    break;
+                }
             }
-        });
-
-        getCoinSecondaryInfo(tickerIdToSearch).then(function (res3){
-            if (res3 != -1) {
-                console.log("Description: " + res3["description"])
-            }else{
-                console.log("Secondary info was not found :(")
+            if (found != true) {
+                //console.log("Ticker not found in database: -1")
+                resolve(-1);
             }
-        });
+        }
+    });
+}
+
+// Save as JSON to provide to the Frontend
+
+var tickerToSearch = "DOT";
+
+var data_to_save = {
+    "name" : null,
+    "ticker": null,
+    "max_supply": null,
+    "circulating_supply": null,
+    "description": null,
+    "price": null,
+    "cmc_rank": null,
+    "percent_change_1h": null,
+    "percent_change_24h": null,
+    "percent_change_7d": null,
+    "percent_change_30d": null
+}
+
+// getTickerIdFromMap(tickerToSearch).then(function (res) {
+//     if (res != -1) {
+//         //console.log(tickerToSearch + "'s CoinMarketCap ID is: " + res);
+//         var tickerIdToSearch = res;
+//         console.log("========================== DATA RECEIVED ==========================")
+//         console.log(" ")
+//         Promise.all([getCoinGeneralInfo(tickerIdToSearch), getCoinSecondaryInfo(tickerIdToSearch), getCoinLatestInfo(tickerIdToSearch)]).then((values) => {
+//             //console.log(values[2]);
+//             if(values[0] != -1 && values[1] != -1 && values[2] != -1){
+//                 data_to_save["name"] = values[0]["name"];
+//                 data_to_save["ticker"] = values[0]["symbol"];
+//                 data_to_save["max_supply"] = values[0]["max_supply"]
+//                 data_to_save["circulating_supply"] = values[0]["circulating_supply"]
+//                 data_to_save["cmc_rank"] = values[0]["cmc_rank"];
+//                 data_to_save["description"] = values[1]["description"]
+//                 data_to_save["price"] = values[2]["quote"]["USD"]["price"]
+//                 data_to_save["percent_change_1h"] = values[2]["quote"]["USD"]["percent_change_1h"]
+//                 data_to_save["percent_change_24h"] = values[2]["quote"]["USD"]["percent_change_24h"]
+//                 data_to_save["percent_change_7d"] = values[2]["quote"]["USD"]["percent_change_7d"]
+//                 data_to_save["percent_change_30d"] = values[2]["quote"]["USD"]["percent_change_30d"]
+//                 console.log(data_to_save)
+//             }
+//         }).catch((reason) => {
+//             console.log(reason);
+//         });
+//     } else {
+//         console.log("Ticker does not exist");
+//     }
+// });
 
 
-    } else {
-        console.log("Ticker does not exist");
-    }
-});
+
