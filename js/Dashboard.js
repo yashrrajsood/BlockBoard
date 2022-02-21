93c0924c62aef7c898f9ff3c9e5dd76c8ca57063
Yash <yashsood978@gmail.com>
2022-02-22 00:47:33 +0800

    API & UI Updates

--- js/Dashboard.js
+++ js/Dashboard.js
@@ -1,10 +1,101 @@
 const params = new Proxy(new URLSearchParams(window.location.search), {
-  get: (searchParams, prop) => searchParams.get(prop),
+    get: (searchParams, prop) => searchParams.get(prop),
 });
-// Get the value of "key" in eg "https://example.com/?key=some_value"
-let TICKER_CODE = params.code; // "some_value"
-
-document.title = TICKER_CODE + " | Blockboard";
+let TICKER_CODE = params.code;
 window.onload = function what() {
-  document.getElementById("titlePage").innerHTML = TICKER_CODE;
+    fetch("http://localhost:3000/coin/" + String(TICKER_CODE))
+        .then((res) => {
+            res.json().then(function (data) {
+                console.log(data);
+                document.title = data["name"] + " | Blockboard";
+                document.getElementById("crypto_logo").src = data["logo"]
+                document.getElementById("titlePage").innerHTML = data["name"] + " (" + data["ticker"] +")";
+                document.getElementById("priceDividerTag").innerHTML = "$" + CurrencyFormatted(data["price"]);
+
+                document.getElementById("oneHourChangeDividerTag").innerHTML = data["percent_change_1h"].toFixed(2) + "%";
+                if(data["percent_change_1h"] < 0){
+                    document.getElementById("oneHourChangeDividerTitle").innerHTML = "1H % 🔻"
+                    document.getElementById("oneHourChangeDividerTag").style.color = "#FF5733"
+                }else{
+                    document.getElementById("oneHourChangeDividerTitle").innerHTML = "1H % 🔺"                    
+                    document.getElementById("oneHourChangeDividerTag").style.color = "#99FF00"
+                }
+
+                document.getElementById("twentyFourHourChangeDividerTag").innerHTML = data["percent_change_24h"].toFixed(2) + "%";
+                if(data["percent_change_24h"] < 0){
+                    document.getElementById("twentyFourHourChangeDividerTitle").innerHTML = "24H % 🔻"
+                    document.getElementById("twentyFourHourChangeDividerTag").style.color = "#FF5733"
+                }else{
+                    document.getElementById("twentyFourHourChangeDividerTitle").innerHTML = "24H % 🔺"
+                    document.getElementById("twentyFourHourChangeDividerTag").style.color = "#99FF00"
+                }
+
+                document.getElementById("sevenDayChangeDividerTag").innerHTML = data["percent_change_7d"].toFixed(2) + "%";
+                if(data["percent_change_7d"] < 0){
+                    document.getElementById("sevenDayChangeDividerTitle").innerHTML = "7D % 🔻"
+                    document.getElementById("sevenDayChangeDividerTag").style.color = "#FF5733"
+                }else{
+                    document.getElementById("sevenDayChangeDividerTitle").innerHTML = "7D % 🔺"
+                    document.getElementById("sevenDayChangeDividerTag").style.color = "#99FF00"
+                }
+
+                document.getElementById("marketCapDividerTag").innerHTML = abbreviateNumber(parseInt(data["market_cap"], 10))
+                document.getElementById("circulatingSupplyDividerTag").innerHTML = abbreviateNumber(parseInt(data["circulating_supply"], 10))
+                document.getElementById("totalSupplyDividerTag").innerHTML = abbreviateNumber(parseInt(data["max_supply"], 10))
+                document.getElementById("rankTag").innerHTML = "#" + data["cmc_rank"];
+                document.getElementById("descriptionDividerTag").innerHTML = data["description"];
+                document.getElementById("hideAll").style.display = "none";
+            });
+        })
+        .catch((err) => {
+            /* handle errors */
+            console.log("ERROR on fetch request", err);
+            document.getElementById("hideAll").style.display = "none";
+            document.getElementById("titlePage").innerHTML = "☹️";
+            document.getElementById("priceTag").innerHTML = "Aw man, an error popped up - \n" + " " + err;
+            
+        });
 };
+
+
+function CurrencyFormatted(amount) {
+    if (amount > 99999.99999){
+        amount = String(amount.toFixed(2));
+        amount = amount.slice(0,3) + "," + amount.slice(3)
+        return amount
+    }
+    else if (amount > 9999.99999){
+        amount = String(amount.toFixed(2));
+        amount = amount.slice(0,2) + "," + amount.slice(2)
+        return amount
+    }
+    else if(amount > 0.009){
+        return String(amount.toFixed(2));
+    }else{
+        return String(amount.toFixed(10));
+    }
+}
+
+function abbreviateNumber(value) {
+    var newValue = value;
+    if (value >= 1000) {
+        var suffixes = ["", "K", "M", "B","T"];
+        var suffixNum = Math.floor( (""+value).length/3 );
+        var shortValue = '';
+        for (var precision = 2; precision >= 1; precision--) {
+            shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+            var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+            if (dotLessShortValue.length <= 2) { break; }
+        }
+        if (shortValue % 1 != 0)  shortValue = shortValue.toFixed(1);
+        newValue = shortValue+suffixes[suffixNum];
+    }
+    return newValue;
+    // var suffixes = ["", "k", "m", "b","t"];
+    // var suffixNum = Math.floor((""+value).length/3);
+    // var shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000,suffixNum)) : value).toPrecision(2));
+    // if (shortValue % 1 != 0) {
+    //     shortValue = shortValue.toFixed(1);
+    // }
+    // return shortValue+suffixes[suffixNum];
+}
--- js/dataRetriever.js
+++ js/dataRetriever.js
@@ -1,20 +1,59 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 3000
+app.use(cors());
+
+var data_to_save = {
+    "name" : null,
+    "ticker": null,
+    "max_supply": null,
+    "circulating_supply": null,
+    "description": null,
+    "price": null,
+    "cmc_rank": null,
+    "logo": null,
+    "percent_change_1h": null,
+    "percent_change_24h": null,
+    "percent_change_7d": null,
+    "percent_change_30d": null,
+    "market_cap": null
+}
+
+var sample_data = {
+    circulating_supply: 13123230,
+    cmc_rank: 1,
+    description: "Bitcoin (BTC) is a cryptocurrency . Users are able to generate BTC through the process of mining. Bitcoin has a current supply of 18,962,493. The last known price of Bitcoin is 40,295.73276291 USD and is down -1.12 over the last 24 hours. It is currently trading on 9168 active market(s) with $21,888,043,886.56 traded over the last 24 hours. More information can be found at https://bitcoin.org/."
+    ,max_supply: 21000000
+    ,name: "Bitcoin"
+    ,percent_change_1h: -0.14056561
+    ,percent_change_7d: 4.57531657
+    ,percent_change_24h: -1.11798969
+    ,percent_change_30d: -4.07603769
+    ,price: 40295.732762907595
+    ,ticker: "BTC"
+    ,logo: "https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"
+    ,market_cap: 728326680331.4109
+}
 
 app.get('/', (req, res) => {
-  res.send('Bitcoin is a cryptocurrency')
+  res.send('Server running as per normal.')
+})
+
+app.get('/coinTEST/:coinID', (req, response) => {
+    response.send(sample_data)
+    console.log("Sent Sample Data for", req.params["coinID"])
 })
 
 app.get('/coin/:coinID', (req, response) => {
+    console.log("REAL Coin Information to Search: ",req.params["coinID"])
     getTickerIdFromMap(req.params["coinID"]).then(function (res) {
         if (res != -1) {
             //console.log(tickerToSearch + "'s CoinMarketCap ID is: " + res);
             var tickerIdToSearch = res;
-            console.log("========================== DATA RECEIVED ==========================")
+            //console.log("========================== DATA RECEIVED ==========================")
             console.log(" ")
             Promise.all([getCoinGeneralInfo(tickerIdToSearch), getCoinSecondaryInfo(tickerIdToSearch), getCoinLatestInfo(tickerIdToSearch)]).then((values) => {
-                //console.log(values[2]);
                 if(values[0] != -1 && values[1] != -1 && values[2] != -1){
                     data_to_save["name"] = values[0]["name"];
                     data_to_save["ticker"] = values[0]["symbol"];
@@ -22,15 +61,20 @@ app.get('/coin/:coinID', (req, response) => {
                     data_to_save["circulating_supply"] = values[0]["circulating_supply"]
                     data_to_save["cmc_rank"] = values[0]["cmc_rank"];
                     data_to_save["description"] = values[1]["description"]
+                    data_to_save["logo"] = values[1]["logo"]
                     data_to_save["price"] = values[2]["quote"]["USD"]["price"]
                     data_to_save["percent_change_1h"] = values[2]["quote"]["USD"]["percent_change_1h"]
                     data_to_save["percent_change_24h"] = values[2]["quote"]["USD"]["percent_change_24h"]
                     data_to_save["percent_change_7d"] = values[2]["quote"]["USD"]["percent_change_7d"]
                     data_to_save["percent_change_30d"] = values[2]["quote"]["USD"]["percent_change_30d"]
+                    data_to_save["market_cap"] = values[2]["quote"]["USD"]["market_cap"]
+                    
                     //console.log(data_to_save)
                 }
-                console.log("Data sent!")
-                response.send('Coin Price: ' + data_to_save["price"])
+                console.log("Data sent for " + values[0]["name"] + " sent!")
+                //console.log(values);
+                response.send(data_to_save)
+                //response.send("Ok soldier")
             }).catch((reason) => {
                 console.log(reason);
             });
@@ -41,6 +85,7 @@ app.get('/coin/:coinID', (req, response) => {
     });
 
   })
+   
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
@@ -217,54 +262,6 @@ function getCoinLatestInfo(id) {
         }
     });
 }
-
-// Save as JSON to provide to the Frontend
-
-var tickerToSearch = "DOT";
-
-var data_to_save = {
-    "name" : null,
-    "ticker": null,
-    "max_supply": null,
-    "circulating_supply": null,
-    "description": null,
-    "price": null,
-    "cmc_rank": null,
-    "percent_change_1h": null,
-    "percent_change_24h": null,
-    "percent_change_7d": null,
-    "percent_change_30d": null
-}
-
-// getTickerIdFromMap(tickerToSearch).then(function (res) {
-//     if (res != -1) {
-//         //console.log(tickerToSearch + "'s CoinMarketCap ID is: " + res);
-//         var tickerIdToSearch = res;
-//         console.log("========================== DATA RECEIVED ==========================")
-//         console.log(" ")
-//         Promise.all([getCoinGeneralInfo(tickerIdToSearch), getCoinSecondaryInfo(tickerIdToSearch), getCoinLatestInfo(tickerIdToSearch)]).then((values) => {
-//             //console.log(values[2]);
-//             if(values[0] != -1 && values[1] != -1 && values[2] != -1){
-//                 data_to_save["name"] = values[0]["name"];
-//                 data_to_save["ticker"] = values[0]["symbol"];
-//                 data_to_save["max_supply"] = values[0]["max_supply"]
-//                 data_to_save["circulating_supply"] = values[0]["circulating_supply"]
-//                 data_to_save["cmc_rank"] = values[0]["cmc_rank"];
-//                 data_to_save["description"] = values[1]["description"]
-//                 data_to_save["price"] = values[2]["quote"]["USD"]["price"]
-//                 data_to_save["percent_change_1h"] = values[2]["quote"]["USD"]["percent_change_1h"]
-//                 data_to_save["percent_change_24h"] = values[2]["quote"]["USD"]["percent_change_24h"]
-//                 data_to_save["percent_change_7d"] = values[2]["quote"]["USD"]["percent_change_7d"]
-//                 data_to_save["percent_change_30d"] = values[2]["quote"]["USD"]["percent_change_30d"]
-//                 console.log(data_to_save)
-//             }
-//         }).catch((reason) => {
-//             console.log(reason);
-//         });
-//     } else {
-//         console.log("Ticker does not exist");
-//     }
-// });
 
 
 
